45fd22a5120082fbf5c44704897d2f8d91971c1e
djk01281 <djk01281@gmail.com>
2022-11-23 06:23:03 +0000

    feat: render Hello World

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div className="App">Hello World</div>;
+  }
+}
+
+export default App;
